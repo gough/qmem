5237fb155dacbf7d6ca907a97786804e4a181cc3
Adam Gough <adam@gough.ca>
2018-02-13 15:58:13 -0500

    Create view asset page

---
+++ resources/views/pages/assets/view.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.no-fluid')
+@section('content')
+	<div class="row">
+		<div class="col-md-12">
+			<div class="pull-left">
+				<h1>View Asset</h1>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-body">
+					View asset
+				</div>
+			</div>
+		</div>
+	</div>
+@stop
