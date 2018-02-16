1e8dc57fe81ea4361dadc8d2123a4558a2edf1fd
Adam Gough <adam@gough.ca>
2018-02-16 13:43:45 -0500

    Add user string to assets table migration

--- database/migrations/2018_02_08_194837_create_assets_table.php
+++ database/migrations/2018_02_08_194837_create_assets_table.php
@@ -17,6 +17,7 @@ class CreateAssetsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('type');
+            $table->string('user');
             $table->timestamps();
         });
     }
