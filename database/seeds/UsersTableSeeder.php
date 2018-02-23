fc4d7634e4705afb342ac73e2aece5f91aff919f
Adam Gough <adam@gough.ca>
2018-02-23 15:24:10 -0800

    Change Users seeder to use new User class

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use \App\User;
+
 use Faker\Factory as Faker;
 
 class UsersTableSeeder extends Seeder
@@ -14,34 +16,36 @@ class UsersTableSeeder extends Seeder
     {
         $faker = Faker::create();
 
-        DB::table('users')->insert([
-            'netid' => '15ag36',
-            'group' => 'admin',
-            'name' => '',
-            'email' => '',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s"),
-        ]);
+        // our first user is important, because it will be the one used on local machines
+        // (when netid system is unavailable)
+        $user = new User;
+        $user->netid = '12ab34';
+        $user->group = 'admin';
+        $user->name = 'Test User';
+        $user->email = 'test.user@example.com';
+        $user->save();
 
-        DB::table('users')->insert([
-            'netid' => '13sjk',
-            'group' => 'admin',
-            'name' => '',
-            'email' => '',
-            'created_at' => date("Y-m-d H:i:s"),
-            'updated_at' => date("Y-m-d H:i:s"),
-        ]);
+        $user = new User;
+        $user->netid = '15ag36';
+        $user->group = 'admin';
+        $user->name = '';
+        $user->email = '';
+        $user->save();
+
+        $user = new User;
+        $user->netid = '13sjk';
+        $user->group = 'admin';
+        $user->name = '';
+        $user->email = '';
+        $user->save();
 
         for ($i = 1; $i < 8; $i++) {
-            $random_date = mt_rand(1400000000, 1500000000);
-            DB::table('users')->insert([
-                'netid' => '',
-                'group' => 'user',
-                'name' => $faker->firstName . ' ' . $faker->lastName,
-                'email' => $faker->email,
-                'created_at' => date("Y-m-d H:i:s", $random_date),
-                'updated_at' => date("Y-m-d H:i:s", $random_date),
-            ]);
+            $user = new User;
+            $user->netid = mt_rand(100000,999999);
+            $user->group = 'user';
+            $user->name = $faker->firstName . ' ' . $faker->lastName;
+            $user->email = $faker->email;
+            $user->save();
         }
     }
 }
