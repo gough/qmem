b2cbdb93dac651442547ac71321f4534c5f1ffec
Adam Gough <adam@gough.ca>
2018-02-25 18:36:16 -0800

    Change dashboard asset colour

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -9,27 +9,27 @@
 						<i class="fa fa-cube fa-4x align-middle"></i>
 					</div>
 					<div class="pull-right">
-						<span class="card-title">{{ number_format($total_assets) }}</span>
-						<span class="card-desc">Total Assets</span>
+						<span class="card-title">{{ number_format($total_items) }}</span>
+						<span class="card-desc">Total Items</span>
 					</div>
 				</div>
-				<a class="card-button-bottom text-primary" href="{{ route('assets.index') }}">
+				<a class="card-button-bottom text-primary" href="#">
 					<span>View details</i></span><i class="pull-right fa fa-arrow-circle-right"></i>
 				</a>
 			</div>
 		</div>
 		<div class="col-lg-3 col-sm-6">
-			<div class="card border-success">
+			<div class="card border-info">
 				<div class="card-info-top bg-info">
 					<div class="pull-left">
 						<i class="fa fa-cube fa-4x align-middle"></i>
 					</div>
 					<div class="pull-right">
-						<span class="card-title">{{ number_format($total_capital) }}</span>
-						<span class="card-desc">Total Capital</span>
+						<span class="card-title">{{ number_format($total_asset) }}</span>
+						<span class="card-desc">Total Assets</span>
 					</div>
 				</div>
-				<a class="card-button-bottom text-success" href="{{ route('assets.capital') }}">
+				<a class="card-button-bottom text-info" href="{{ route('assets.index') }}">
 					<span>View details</i></span><i class="pull-right fa fa-arrow-circle-right"></i>
 				</a>
 			</div>
@@ -42,10 +42,10 @@
 					</div>
 					<div class="pull-right">
 						<span class="card-title">{{ number_format($total_consumable) }}</span>
-						<span class="card-desc">Total Consumable</span>
+						<span class="card-desc">Total Consumables</span>
 					</div>
 				</div>
-				<a class="card-button-bottom text-success" href="{{ route('assets.consumable') }}">
+				<a class="card-button-bottom text-success" href="{{ route('consumables.index') }}">
 					<span>View details</i></span><i class="pull-right fa fa-arrow-circle-right"></i>
 				</a>
 			</div>
@@ -61,48 +61,56 @@
 						<span class="card-desc">Total Users</span>
 					</div>
 				</div>
-
-
-
-
 				<a class="card-button-bottom text-danger" href="{{ route('user.index') }}">
 					<span>View details</i></span><i class="pull-right fa fa-arrow-circle-right"></i>
-
-
-
-					
 				</a>
 			</div>
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-12">
+		<div class="col-md-6">
 			<div class="card">
 				<div class="card-header">
-					<span class="card-title">Recent Activity</span>
+					<span class="card-title">Recent Asset Activity</span>
 				</div>
 				<div class="card-body">
 					<table class="table table-bordered table-hover table-responsive-md">
 						<thead class="thead-default">
 							<tr>
-								<th style="width: 25%">Date</th>
-								<th style="width: 20%">User</th>
-								<th style="width: 15%">Action</th>
-								<th style="width: 40%">Item</th>
-								<!--<th class="hidden-xs" style="width: 1%" colspan="2">Item</th>
-								<th style="width: 44%">Item</th>-->
+								<th>Date</th>
+								<th>Item</th>
 							</tr>
 						</thead>
 						<tbody>
 							@foreach ($recent_assets as $asset)
 							<tr>
 								<td>{{ $asset->updated_at }}</td>
-								<td><a href="#">{{ $asset->user }}</a></td>
-								<td>Update</td>
-								<td>
-									<i class="fa {{ $asset->type == 'consumable' ? 'fa-tint' : 'fa-cube' }}"></i>
-									<a href="#">{{ $asset->name }}</a>
-								</td>
+								<td>{{ $asset->name }}</td>
+							</tr>
+							@endforeach
+						</tbody>						
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="card">
+				<div class="card-header">
+					<span class="card-title">Recent Consumable Activity</span>
+				</div>
+				<div class="card-body">
+					<table class="table table-bordered table-hover table-responsive-md">
+						<thead class="thead-default">
+							<tr>
+								<th>Date</th>
+								<th>Item</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach ($recent_consumables as $consumable)
+							<tr>
+								<td>{{ $consumable->updated_at }}</td>
+								<td>{{ $consumable->name }}</td>
 							</tr>
 							@endforeach
 						</tbody>						
