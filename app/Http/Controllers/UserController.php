f3755133d311cc90614c20f7c4cf212b9374fcf2
Adam Gough <adam@gough.ca>
2018-02-27 15:41:56 -0800

    Update UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,23 +2,133 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Http\Request;
 use Auth;
 
 class UserController extends Controller
 {
-    public function index() {
-        $users = User::paginate(20);
-        return view ('pages.user.index', compact('users'));
-    }
-    public function preferences(Request $request) {
-        dd($request->user());
-    	return view('pages.user.preferences');
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // GET /users
+        
+        $users = User::orderBy('created_at', 'desc')->paginate(50);
+
+        return view('pages.users.index', compact('users'));
     }
 
-    public function logout() {
+    public function preferences(Request $request)
+    {
+        return view('pages.user.preferences');
+    }
+
+    public function logout()
+    {
         Auth::logout();
-		return redirect('https://idptest.queensu.ca/idp/profile/Logout');
+        return redirect('https://idptest.queensu.ca/idp/profile/Logout');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // GET /users/new
+        
+        return view('pages.users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // POST /users
+        
+        // TODO: actually store user
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\user  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function view($netid)
+    {
+        // GET /users/{netid}
+                
+        $id = User::where('netid', $netid)->firstOrFail()->id;
+        $user = User::findOrFail($id);
+
+        return view('pages.users.view', compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\user  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        // GET /users/{id}/edit
+        
+        $user = ''; // TODO: actually load user
+
+        return view('pages.users.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\user  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, user $user)
+    {
+        // POST /users/{id}
+        
+        // TODO: actually update user
+    }
+
+    /**
+     * Show the form for deleting the specified resource.
+     *
+     * @param  \App\user  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function delete(User $user)
+    {
+        // GET /users/{id}/delete
+
+        $user = ''; // TODO: actually load user
+
+        return view('pages.users.delete', compact('user'));        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\user  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        // POST /users/{id}
+        
+        // TODO: actually destory user
     }
 }
+
