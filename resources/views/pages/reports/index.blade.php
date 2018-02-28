0a93e224c68186bb00571698f1a82de9c616f7e9
Adam Gough <adam@gough.ca>
2018-02-28 12:56:24 -0500

    Update Reports page

--- resources/views/pages/reports/index.blade.php
+++ resources/views/pages/reports/index.blade.php
@@ -1,4 +1,19 @@
 @extends('layouts.default')
 @section('content')
-	<h1>Reports</h1>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="pull-left">
+				<h1>Reports</h1>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-body">
+					Reports
+				</div>
+			</div>
+		</div>
+	</div>
 @stop
