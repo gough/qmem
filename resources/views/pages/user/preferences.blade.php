b3e66a7deca56e59d6e7808ce71d1571ba924eca
Adam Gough <adam@gough.ca>
2018-02-28 12:56:14 -0500

    Update user preferences page

--- resources/views/pages/user/preferences.blade.php
+++ resources/views/pages/user/preferences.blade.php
@@ -1,4 +1,19 @@
-@extends('layouts.default')
+@extends('layouts.no-fluid')
 @section('content')
-	<h1>Preferences</h1>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="pull-left">
+				<h1>Preferences</h1>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-body">
+					{{ Auth::user() }}
+				</div>
+			</div>
+		</div>
+	</div>
 @stop
