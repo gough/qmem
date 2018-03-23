9b38094ec8b8b050fe94e0a2cabff4d82d9aa787
Adam Gough <adam@gough.ca>
2018-03-23 16:27:11 -0400

    Fix search app ID

--- resources/views/includes/foot.blade.php
+++ resources/views/includes/foot.blade.php
@@ -10,7 +10,7 @@
 <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
 <script>
-  	var client = algoliasearch('{{ env('ALGOLIA_APP_ID') }}', '{{ env('ALOGLIA_SEARCH_ONLY_SECRET') }}');
+  	var client = algoliasearch('{{ config('algolia.app.id') }}', '{{ config('algolia.search.only.secret') }}');
   	var assets = client.initIndex('assets');
 	var consumables = client.initIndex('consumables');
 
