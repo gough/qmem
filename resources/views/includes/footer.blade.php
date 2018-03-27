968644d2e1b7532fa6abe60c71ede5411d21b249
Adam Gough <adam@gough.ca>
2018-03-27 11:09:09 -0400

    Add version tracking

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,6 +3,6 @@
 		<span class="text-muted">Made for <a class="" href="http://www.cs.queensu.ca/students/undergraduate/courses/desc/CISC-498.html">CISC 498</a> by AG, AN, JH, LR, and SS</span>
 	</div>
 	<div class="pull-right">
-		<span class="text-muted text-right">Version 1.0.0</span>
+		<span class="text-muted text-right">QMEM @version('full')</span>
 	</div>
 </div>
